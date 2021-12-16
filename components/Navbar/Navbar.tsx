241a2cf611ddc42b5ca27558574172de4b1c0e40
nemanjam <nemanja.mitic.elfak@hotmail.com>
2021-12-16 19:06:29 +0100

    all navitems as object

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -13,179 +13,269 @@ import { useViewport } from 'components/hooks';
 const isActive: (router: NextRouter, pathname: string) => boolean = (router, pathname) =>
   router.pathname === pathname;
 
-const getNavLinks = (router: NextRouter, session: Session, isMobile: boolean) => (
-  <>
-    {isMobile ? (
-      <>
-        {session ? (
-          <>
-            <Link href={Routes.SITE.HOME}>
-              <a className="bold" data-active={isActive(router, Routes.SITE.HOME)}>
-                Home
-              </a>
-            </Link>
+interface ItemsArgs {
+  router: NextRouter;
+  session: Session;
+  onHamburgerClick?: () => void;
+  mobileMenuOpen?: boolean;
+  isGetDropdownItems?: boolean;
+}
 
-            <Link
-              href={{
-                pathname: '/[username]',
-                query: { username: session.user.username },
-              }}
-            >
-              <a
-                className="bold"
-                data-active={isActive(router, `/${session.user.username}`)}
-              >
-                Profile
-              </a>
-            </Link>
+const getAllItems = ({
+  router,
+  session,
+  onHamburgerClick,
+  mobileMenuOpen,
+  isGetDropdownItems,
+}: ItemsArgs) => ({
+  home: (
+    <Link href={Routes.SITE.HOME}>
+      <a className="bold" data-active={isActive(router, Routes.SITE.HOME)}>
+        Home
+      </a>
+    </Link>
+  ),
+  profile: session && (
+    <Link
+      href={{
+        pathname: '/[username]',
+        query: { username: session.user.username },
+      }}
+    >
+      <a className="bold" data-active={isActive(router, `/${session.user.username}`)}>
+        Profile
+      </a>
+    </Link>
+  ),
+  drafts: (
+    <Link href={Routes.SITE.DRAFTS}>
+      <a data-active={isActive(router, Routes.SITE.DRAFTS)}>My drafts</a>
+    </Link>
+  ),
+  settings: (
+    <Link href={Routes.SITE.SETTINGS}>
+      <a data-active={isActive(router, Routes.SITE.SETTINGS)}>Settings</a>
+    </Link>
+  ),
+  logout: (
+    <button onClick={() => signOut()}>
+      <a>Log out</a>
+    </button>
+  ),
+  login: (
+    <Link href={Routes.SITE.LOGIN}>
+      <a>Log in</a>
+    </Link>
+  ),
+  register: (
+    <Link href={Routes.SITE.REGISTER}>
+      <a>Register</a>
+    </Link>
+  ),
+  // prevent recursion
+  avatar: !isGetDropdownItems && session && (
+    <Dropdown
+      items={getDropdownItems({ router, session, onHamburgerClick, mobileMenuOpen })}
+    >
+      <img src={getAvatarPath(session.user)} width="50" height="50" />
+    </Dropdown>
+  ),
+  hamburger: (
+    <button
+      type="button"
+      aria-label="Toggle mobile menu"
+      onClick={onHamburgerClick}
+      className="rounded md:hidden focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-50"
+    >
+      <RiMenuLine
+        className={`transition duration-100 ease h-8 w-8 ${
+          mobileMenuOpen ? 'transform rotate-90' : ''
+        }`}
+      />
+    </button>
+  ),
+});
 
-            <Link href={Routes.SITE.DRAFTS}>
-              <a data-active={isActive(router, Routes.SITE.DRAFTS)}>My drafts</a>
-            </Link>
+// loged in
+// left desktop navitems: home, profile, drafts
+// right desktop: avatar
+// left mobile: none
+// dropdown desktop: settins, logout
+// dropdown mobile: no dropdown
 
-            <Link href={Routes.SITE.SETTINGS}>
-              <a data-active={isActive(router, Routes.SITE.SETTINGS)}>Settings</a>
-            </Link>
+// loged out
+// left desktop: home
+// right: login, register
+// left mobile: none
 
-            <button onClick={() => signOut()}>
-              <a>Log out</a>
-            </button>
-          </>
-        ) : (
-          <>
-            <Link href={Routes.SITE.LOGIN}>
-              <a>Log in</a>
-            </Link>
-            <Link href={Routes.SITE.REGISTER}>
-              <a>Register</a>
-            </Link>
-          </>
-        )}
-      </>
-    ) : (
-      <>
-        {session ? (
-          <>
-            <Link href={Routes.SITE.HOME}>
-              <a className="bold" data-active={isActive(router, Routes.SITE.HOME)}>
-                Home
-              </a>
-            </Link>
+// left, loggedin, mobile, dropdown
 
-            <Link
-              href={{
-                pathname: '/[username]',
-                query: { username: session.user.username },
-              }}
-            >
-              <a
-                className="bold"
-                data-active={isActive(router, `/${session.user.username}`)}
-              >
-                Profile
-              </a>
-            </Link>
+const objectFilter = (obj, predicate) =>
+  Object.fromEntries(Object.entries(obj).filter(predicate));
 
-            <Link href={Routes.SITE.DRAFTS}>
-              <a data-active={isActive(router, Routes.SITE.DRAFTS)}>My drafts</a>
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link href={Routes.SITE.LOGIN}>
-              <a>Log in</a>
-            </Link>
-            <Link href={Routes.SITE.REGISTER}>
-              <a>Register</a>
-            </Link>
-          </>
-        )}
-      </>
-    )}
-  </>
-);
+interface FilterItemsArgs extends ItemsArgs {
+  argsArray: string[];
+}
 
-const getDropdownItems = (router: NextRouter, session: Session) =>
-  session
-    ? [
-        <Link href={Routes.SITE.SETTINGS}>
-          <a data-active={isActive(router, Routes.SITE.SETTINGS)}>Settings</a>
-        </Link>,
+const filterAllItems = ({ argsArray, ...restArgs }: FilterItemsArgs) =>
+  argsArray?.length > 0
+    ? Object.values(
+        objectFilter(getAllItems(restArgs), ([key, value]) => argsArray.includes(key))
+      )
+    : [];
 
-        <button onClick={() => signOut()}>
-          <a>Log out</a>
-        </button>,
-      ]
-    : [
-        <Link href={Routes.SITE.LOGIN}>
-          <a>Log in</a>
-        </Link>,
+interface GetNavLinksArgs extends ItemsArgs {
+  isMobile: boolean;
+}
 
-        <Link href={Routes.SITE.REGISTER}>
-          <a>Register</a>
-        </Link>,
-      ];
+const getLeftNavLinks = ({ router, session, isMobile }: GetNavLinksArgs) => {
+  const leftNavLinksDesktopLoggedIn = ['home', 'profile', 'drafts'];
+  const leftNavLinksMobile = [];
+  const leftNavLinksDesktopLoggedOut = ['home'];
+
+  let argsArray = [];
+  if (!isMobile && session) argsArray = leftNavLinksDesktopLoggedIn;
+  if (!isMobile && !session) argsArray = leftNavLinksDesktopLoggedOut;
+  if (isMobile) argsArray = leftNavLinksMobile;
+
+  return filterAllItems({ router, session, argsArray });
+};
+
+const getRightNavLinks = ({
+  router,
+  session,
+  isMobile,
+  onHamburgerClick,
+  mobileMenuOpen,
+}: GetNavLinksArgs) => {
+  const rightNavLinksDesktopLoggedIn = ['avatar'];
+  const rightNavLinksMobile = ['hamburger'];
+  const rightNavLinksDesktopLoggeOut = ['login', 'register'];
+
+  let argsArray = [];
+  if (!isMobile && session) argsArray = rightNavLinksDesktopLoggedIn;
+  if (!isMobile && !session) argsArray = rightNavLinksDesktopLoggeOut;
+  if (isMobile) argsArray = rightNavLinksMobile;
+
+  return filterAllItems({ router, session, argsArray, onHamburgerClick, mobileMenuOpen });
+};
+
+const getMobileNavLinks = (args: GetNavLinksArgs) => {
+  const navLinksMobileLoggedIn = [
+    'home',
+    'profile',
+    'drafts',
+    'settings',
+    'avatar',
+    'logout',
+  ];
+
+  const navLinksMobileLoggedOut = ['home', 'login', 'register'];
+};
+
+function getDropdownItems(args: ItemsArgs) {
+  const dropdownDesktopLoggedIn = ['settings', 'logout'];
+  const dropdownMobile = [];
+  const dropdownLoggedOut = [];
+
+  let argsArray = [];
+  if (args?.session) argsArray = dropdownDesktopLoggedIn;
+  if (!args?.session) argsArray = dropdownLoggedOut;
+
+  return filterAllItems({ ...args, argsArray, isGetDropdownItems: true });
+}
 
 const Navbar: React.FC = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const router = useRouter();
   const { width } = useViewport();
   const isMobile = width < 768;
 
   const { data: session, status } = useSession();
+  const _onHamburgerClick = () => setMobileMenuOpen(!mobileMenuOpen);
+
+  const mobileMenu =
+    mobileMenuOpen &&
+    getAllNavLinks({
+      router,
+      session,
+      isMobile,
+      mobileMenuOpen,
+      onHamburgerClick: _onHamburgerClick,
+    });
+
+  console.log('mobileMenu', mobileMenu);
 
   return (
     <div className="bg-gradient-to-r from-blue-300 to-blue-100">
       <DesktopNavbar
-        menuOpen={menuOpen}
-        setMenuOpen={setMenuOpen}
+        mobileMenuOpen={mobileMenuOpen}
+        setMobileMenuOpen={setMobileMenuOpen}
         session={session}
         router={router}
         isMobile={isMobile}
       />
-      {menuOpen && <MobileMenu>{getNavLinks(router, session, isMobile)}</MobileMenu>}
+      {mobileMenuOpen && <MobileMenu>{mobileMenu}</MobileMenu>}
     </div>
   );
 };
 
-const DesktopNavbar = ({ menuOpen, setMenuOpen, session, router, isMobile }) => (
-  <div className="flex justify-between h-14 px-4">
-    <div className="flex">
-      <div className="flex items-center gap-2">
-        <FaCat className="h-10 w-10 text-violet-500" />
-        <a
-          href="#home"
-          className="text-xl font-bold no-underline text-gray-800 hover:text-gray-600"
-        >
-          NP Boilerplate
-        </a>
+const DesktopNavbar = ({
+  mobileMenuOpen,
+  setMobileMenuOpen,
+  session,
+  router,
+  isMobile,
+}) => {
+  const _onHamburgerClick = () => setMobileMenuOpen(!mobileMenuOpen);
+
+  return (
+    <div className="flex justify-between h-14 px-4">
+      <div className="flex">
+        <div className="flex items-center gap-2">
+          <FaCat className="h-10 w-10 text-violet-500" />
+          <a
+            href="#home"
+            className="text-xl font-bold no-underline text-gray-800 hover:text-gray-600"
+          >
+            NP Boilerplate
+          </a>
+        </div>
+
+        <nav className="hidden md:flex items-center space-x-6 h-full">
+          {getLeftNavLinks({ router, session, isMobile })}
+        </nav>
       </div>
 
-      <nav className="hidden md:flex items-center space-x-6 h-full">
-        {getNavLinks(router, session, isMobile)}
-      </nav>
+      {/* render avatar */}
+      {!isMobile &&
+        session &&
+        getAllItems({
+          router,
+          session,
+        })?.avatar}
+
+      {/* render hamburger */}
+      {isMobile &&
+        getAllItems({
+          router,
+          session,
+          mobileMenuOpen,
+          onHamburgerClick: _onHamburgerClick,
+        })?.hamburger}
+
+      {/* render login/register */}
+      {!session &&
+        getRightNavLinks({
+          router,
+          session,
+          isMobile,
+          mobileMenuOpen,
+          onHamburgerClick: _onHamburgerClick,
+        })}
     </div>
-    {!isMobile && session && (
-      <div className="flex space-x-6">
-        <Dropdown items={getDropdownItems(router, session)}>
-          <img src={getAvatarPath(session.user)} width="50" height="50" />
-        </Dropdown>
-      </div>
-    )}
-    <button
-      type="button"
-      aria-label="Toggle mobile menu"
-      onClick={() => setMenuOpen(!menuOpen)}
-      className="rounded md:hidden focus:outline-none focus:ring focus:ring-gray-500 focus:ring-opacity-50"
-    >
-      <RiMenuLine
-        className={`transition duration-100 ease h-8 w-8 ${
-          menuOpen ? 'transform rotate-90' : ''
-        }`}
-      />
-    </button>
-  </div>
-);
+  );
+};
 
 const MobileMenu = ({ children }) => (
   <nav className="p-4 flex flex-col space-y-3 md:hidden">{children}</nav>
