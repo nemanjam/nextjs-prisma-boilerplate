fc616eb58a141dd8fa512f44b74bbe6b1b603c07
nemanjam <nemanja.mitic.elfak@hotmail.com>
2022-01-02 19:58:10 +0100

    seed db in Dockerfile.prod

--- next.config.js
+++ next.config.js
@@ -13,4 +13,10 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
