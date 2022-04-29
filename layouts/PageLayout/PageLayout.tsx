646d83f1b75f50e90cd4f003a7e1a6e55a6b5762
nemanjam <nemanja.mitic.elfak@hotmail.com>
2022-04-29 14:05:45 +0200

    remove unwanted Suspense

--- layouts/PageLayout/PageLayout.tsx
+++ layouts/PageLayout/PageLayout.tsx
@@ -1,10 +1,9 @@
-import React, { FC, ReactNode, Suspense } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { withBem } from 'utils/bem';
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
 import SuspenseWrapper from 'lib-client/providers/SuspenseWrapper';
 import MeProvider from 'lib-client/providers/Me';
-import Loading from 'components/Loading';
 
 type Props = {
   children: ReactNode;
@@ -15,23 +14,21 @@ const PageLayout: FC<Props> = ({ children, noPaddingTop }) => {
   const b = withBem('page-layout');
 
   return (
-    <Suspense fallback={<Loading loaderType="screen" />}>
-      <MeProvider>
-        <div className={b()}>
-          <Navbar />
-          <div className={b('navbar-placeholder')} />
+    <MeProvider>
+      <div className={b()}>
+        <Navbar />
+        <div className={b('navbar-placeholder')} />
 
-          <main className={b('content', { 'no-padding-top': noPaddingTop })}>
-            {/* Views (page) level loading and error handling*/}
-            <SuspenseWrapper errorFallbackType="page" loaderType="page">
-              {children}
-            </SuspenseWrapper>
-          </main>
+        <main className={b('content', { 'no-padding-top': noPaddingTop })}>
+          {/* Views (page) level loading and error handling*/}
+          <SuspenseWrapper errorFallbackType="page" loaderType="page">
+            {children}
+          </SuspenseWrapper>
+        </main>
 
-          <Footer />
-        </div>
-      </MeProvider>
-    </Suspense>
+        <Footer />
+      </div>
+    </MeProvider>
   );
 };
 
