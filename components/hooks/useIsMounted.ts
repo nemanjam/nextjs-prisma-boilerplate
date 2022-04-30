78a01c998c0795a58f588b30bd0aad19fe4f1f30
nemanjam <nemanja.mitic.elfak@hotmail.com>
2022-04-30 10:06:37 +0200

    update ts-jest to v28-next

--- components/hooks/useIsMounted.ts
+++ components/hooks/useIsMounted.ts
@@ -3,6 +3,9 @@ import { useEffect, useState } from 'react';
 // https://github.com/vercel/next.js/discussions/17443
 // must be like this for Next.js SSR server state === client state
 
+/**
+ * isMounted === !isSSR
+ */
 const useIsMounted = () => {
   const [isMounted, setIsMounted] = useState(false);
 
