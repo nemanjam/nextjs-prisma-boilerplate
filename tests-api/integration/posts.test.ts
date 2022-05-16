967a252a22204f11bcbe0b885e429ed79e0dce39
nemanjam <nemanja.mitic.elfak@hotmail.com>
2022-05-16 18:24:37 +0200

    update post integration test works

--- tests-api/integration/posts.test.ts
+++ tests-api/integration/posts.test.ts
@@ -3,7 +3,7 @@ import indexHandler from 'pages/api/posts';
 import idHandler from 'pages/api/posts/[id]';
 import { Routes } from 'lib-client/constants';
 import { fakePostWithAuthor } from 'test-client/server/fake-data';
-import { PostCreateData } from 'types/models/Post';
+import { PostCreateData, PostUpdateData } from 'types/models/Post';
 import prisma from 'lib-server/prisma';
 import { createUser } from 'lib-server/services/users';
 import { ClientUser } from 'types/models/User';
@@ -65,6 +65,63 @@ describe('Posts', () => {
       expect.objectContaining({
         title,
         content,
+        // match nested author
+        author: expect.objectContaining({
+          id: author.id,
+        }),
+      })
+    );
+
+    // getMe called 2 times
+    expect(mockedGetMeService).toHaveBeenCalled();
+  });
+
+  test('update post success', async () => {
+    // prepare controller args
+    fakePostWithAuthor.author = createdUser;
+
+    const { author, title: createdTitle, content } = fakePostWithAuthor;
+    const createPostData: PostCreateData = { title: createdTitle, content };
+
+    // prepare, create post in db with id
+    const request = testClient(indexHandler);
+    const { body: post } = await request.post(Routes.API.POSTS).send(createPostData);
+
+    // prepare args
+    const title = 'Updated title';
+    const updatePostData: PostUpdateData = { title };
+
+    // act
+    // pass id like this again
+    const requestId = testClient(idHandler, { id: post.id.toString() });
+    // post owner, it should work
+    const { statusCode, body } = await requestId
+      .patch(Routes.API.POSTS)
+      .send(updatePostData);
+
+    // query db
+    const updatedPostWithAuthor = await prisma.post.findFirst({
+      where: { title },
+      include: { author: true },
+    });
+
+    // assert http response
+    expect(statusCode).toBe(200);
+    // match response
+    expect(body).toEqual(
+      expect.objectContaining({
+        title,
+        // match nested author
+        author: expect.objectContaining({
+          id: author.id,
+        }),
+      })
+    );
+
+    // assert db entry
+    expect(updatedPostWithAuthor).toEqual(
+      expect.objectContaining({
+        title,
         // match nested author
         author: expect.objectContaining({
           id: author.id,
