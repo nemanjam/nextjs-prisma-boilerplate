3bec7e7a9c3641f866e758265a9dfe67f13ffbcc
nemanjam <nemanja.mitic.elfak@hotmail.com>
2022-06-18 09:04:10 +0200

    views tests only

--- test-client/config/jest.config.js
+++ test-client/config/jest.config.js
@@ -8,10 +8,9 @@ const jestConfig = {
   setupFiles: ['jsdom-worker', '<rootDir>/jest.env.setup.ts'], // defines URL.createObjectURL
   setupFilesAfterEnv: ['<rootDir>/test-client/config/jest.setup.ts'],
   testMatch: [
-    '<rootDir>/components/**/ThemeChanger.test.tsx',
     // '<rootDir>/components/**/*.test.{ts,tsx}', // whitelist only
     // '<rootDir>/lib-client/**/*.test.{ts,tsx}',
-    // '<rootDir>/views/**/*.test.{ts,tsx}',
+    '<rootDir>/views/**/*.test.{ts,tsx}',
   ],
   // remove when fixed in react-hook-form/resolvers/zod
   resolver: '<rootDir>/test-client/config/hook-form-resolver-fix.js',
